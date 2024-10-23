f2f360b886fb3fb0235f07a0c792c644ec322f14
PrathamDwivedi27 <prathamd549@gmail.com>
2024-10-23 17:05:22 +0530

    [Feat]: Complete the view-story page with react-page-flip library

--- app/view-story/[id]/page.tsx
+++ app/view-story/[id]/page.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+"use client"
+import { db } from '@/config/db'
+import { StoryData } from '@/config/schema'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { eq } from 'drizzle-orm'
+import HTMLFlipBook from 'react-pageflip';
+import BookCoverPage from '../_components/BookCoverPage'
+import StoryPages from '../_components/StoryPages'
+import LastPage from '../_components/LastPage'
 
-const ViewStory = () => {
+const ViewStory = ({params}:any) => {
+
+    const [story,setStory]=useState<any>();
+    
+
+    useEffect(() => {
+        console.log(params.id)
+        getStory();
+    }, [])
+
+    const getStory=async ()=>{
+        const result=await db.select().from(StoryData)
+        .where(eq(StoryData.storyId,params.id));
+
+        console.log(result[0]);
+        setStory(result[0]);
+    }
   return (
-    <div>
-      
+    <div className='p-10 md:px-20 lg:px-40'>
+      <h2 className='font-bold text-4xl text-center p-10 bg-primary text-white'>{story?.output?.story_name}</h2>
+
+        {/* @ts-ignore */}
+      <HTMLFlipBook width={500} height={500}
+        showCover={true}
+        className='mt-10'
+      >
+        <div>
+        <BookCoverPage imageUrl={story?.coverImage}/>
+        </div>
+
+        {
+            [...Array(story?.output?.chapters?.length)].map((item,index)=>(
+                <div key={index} className='bg-white p-10 border'>
+                    <StoryPages storyChapter={story?.output?.chapters[index]}/>
+
+                </div>
+            ))
+        }
+
+        <div>
+            <LastPage/>
+        </div>
+            
+        </HTMLFlipBook>
     </div>
   )
 }
---
+++ app/view-story/_components/LastPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LastPage = () => {
+  return (
+    <div className='bg-primary p-10 h-full'>
+      <h2 className='text-start text-2xl font-bold text-white'>The End</h2>
+      <p className='text-xl p-10 mt-3 rounded-lg bg-slate-100 text-center'>
+        Thank you for reading the story! We hope you enjoyed it.
+      </p>
+    </div>
+  );
+};
+
+export default LastPage;
